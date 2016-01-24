4b0f10f23a18ab3eeb9ff4c479691868112db084
Rene Clausen Nielsen <r@rcn.dk>
2016-01-24 15:54:35 +0545

    Updated to use Python Twitter Tools

--- getTweetsByID.py
+++ getTweetsByID.py
@@ -1,11 +1,15 @@
 """
-NOTE: You must have python-twitter installed on the machine running this script;
-	  You can install it by running this on the command line:
+NOTE: You must have Python Twitter Toolbox (https://github.com/sixohsix/twitter)
+installed on the machine running this script. You can install it using
+setuptools/easy_install (https://pypi.python.org/pypi/setuptools)
 
-	  			sudo pip install python-twitter
+Run this on the command line:
+
+	  			easy_install twitter
 """
 
-import twitter, csv, json
+from twitter import *
+import csv, json
 
 # Twitter OAuth Credentials
 # Get these be creating a new Twitter app on https://apps.twitter.com/app/new, and look
@@ -16,23 +20,24 @@ access_token = "" #Access Token
 access_secret = "" #Access Token Secret
 
 # Establish Twitter OAuth connection
-api = twitter.Api(consumer_key, consumer_secret, access_token, access_secret)
+t = Twitter(auth=OAuth(access_token, access_secret, consumer_key, consumer_secret))
 
 # Path on your machine to the CSV file holding Tweet IDs.
-csvpath = ''
+# Example, Mac: /Users/[username]/Documents/twitter-analysis/src/TweetIDs.csv
+csvpath = ""
 
-# Path on your machine to where the output file should be.
-tweetspath = ''
+# Path on your machine to where the output JSON file should be.
+# Example, Mac: /Users/[username]/Documents/twitter-analysis/data/tweets.json
+tweetspath = ""
 
 # Get tweets from Twitter API by sending list of Twitter IDs, then write to JSON file.
 # Each object is separated by a newline character for easier reading.
 with open(csvpath, 'rb') as csvfile: #Get ready to read Tweet ID file
 	with open(tweetspath, 'wb') as tweetsfile: #Get ready to write to output file
-		tweetreader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
-		next(tweetreader)
+		tweetreader = csvfile.readlines()
 		for row in tweetreader:
-				try:
-					data = api.GetStatus(int(row[0])) #Get each tweet
-					tweetsfile.write(data.AsJsonString() + '\n') #Write each tweet
-				except: #For tweets that have been deleted since you collected the IDs
-					pass
+			try:
+				data = t.statuses.lookup(_id=row) #Get each tweet
+				json.dump(data, tweetsfile) #Write each tweet
+			except: #Should something not work (blank line, only deleted tweets in line)
+				pass
